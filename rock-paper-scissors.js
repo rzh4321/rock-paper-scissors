0429b2284a803cc9f2e6b9c5a00ddb969f85fbe0
Ricky <rzh4321@gmail.com>
2023-03-10 20:11:39 -0500

    finished game

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -5,16 +5,27 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase()
+    let s = new Set(['rock', 'paper', 'scissors']);
+    if (!s.has(playerSelection)) {
+        return "not a valid choice";
+    }
     if (playerSelection == computerSelection) {
-        return `TIE! ${playerSelection.toUpperCase} ties with ${computerSelection.toUpperCase}`
+        return `TIE! ${playerSelection.toUpperCase} ties with ${computerSelection.toUpperCase}`;
     }
     if ((playerSelection == 'rock' && computerSelection == 'scissors') || 
     (playerSelection == 'paper' && computerSelection == 'rock') ||
     (playerSelection == 'scissors' && computerSelection == 'paper')) {
-        return `You win! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}`
+        return `You win! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}`;
     }
     else {
-        return `You lose! ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}`
+        return `You lose! ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}`;
     }
 
 }
+
+function game() {
+    let playerSelection = prompt("enter rock, paper, or scissors:", 'roCk');
+    console.log(playRound(playerSelection, getComputerChoice()));
+}
+
+game();
