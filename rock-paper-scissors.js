6582c070286f36d56a747957f5f92f9e9e0329a3
Ricky <rzh4321@gmail.com>
2023-03-10 19:54:42 -0500

    completed playRound

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,3 +2,19 @@ function getComputerChoice() {
     let index = Math.floor(Math.random() * 3);
     return ['rock','paper','scissors'][index];
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase()
+    if (playerSelection == computerSelection) {
+        return `TIE! ${playerSelection.toUpperCase} ties with ${computerSelection.toUpperCase}`
+    }
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') || 
+    (playerSelection == 'paper' && computerSelection == 'rock') ||
+    (playerSelection == 'scissors' && computerSelection == 'paper')) {
+        return `You win! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}`
+    }
+    else {
+        return `You lose! ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}`
+    }
+
+}
